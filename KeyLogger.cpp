c1ffd57049749ada07e98deaf10a9343b641b2f4
Travis <travishill62102@gmail.com>
2022-08-17 01:03:52 -0400

    the plan :)

--- KeyLogger.cpp
+++ KeyLogger.cpp
@@ -1,4 +1,27 @@
+#include <windows.h>
+#include <iostream>
+#include <fstream>
+
+
+void fileData() {
+    //put keystrokes into text file
+}
+
+std::string specialKey(int keyPressed) {
+    //Space
+    //Enter
+    //Tab
+    //Caps Lock
+    //Back Space
+    //Shift
+    //Control
+    //Alt
+    return "ah";
+}
 
 int main() {
+    //Loop through each key using ASCII values.
+    //Check if the key is pressed
+    //If its a special key, translate it
     return 1;
 }
